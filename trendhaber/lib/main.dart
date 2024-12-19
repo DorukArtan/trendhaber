4629f16a675af3982f1878df1d9e9d81de48e8aa
Berke <berkedurdu19@gmail.com>
2024-12-19 15:54:11 +0300

    added bottom navigation bar

--- trendhaber/lib/main.dart
+++ trendhaber/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/settings_page.dart';
+import 'pages/feed_page.dart'; // Import the FeedPage
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
-  return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'TrendHaber',
-    theme: ThemeData(
-      primarySwatch: Colors.pink,
-    ),
-    home: SettingsPage(),
-  );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TrendHaber',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: HomePage(), // Set HomePage as the home page
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    FeedPage(),
+    SettingsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TrendHaber'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Feed',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.pink,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
