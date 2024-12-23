7af9c0f5b7206932a9818ff5a45082a6aea1f897
DorukArtan <medoartan@gmail.com>
2024-12-23 20:01:35 +0300

    added news page(when clicked on carousel view it will show the larger version of it) added navigation to feed page
    KNOWN ISSUES
    
    The detailed news is not good enough the picture isnt big enough or wide enough it should be bigger and wider i dont have the time to do it rn will return back to it later

--- trendhaber/lib/pages/feed_page.dart
+++ trendhaber/lib/pages/feed_page.dart
@@ -4,6 +4,7 @@ import 'package:trendhaber/news_fetcher.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:trendhaber/provider.dart';
 import 'package:trendhaber/providers/bookmark_provider.dart';
+import 'package:trendhaber/pages/news_page.dart';
 
 class FeedPage extends ConsumerStatefulWidget {
   @override 
@@ -48,60 +49,70 @@ class _FeedPageState extends ConsumerState<FeedPage> {
               itemBuilder: (BuildContext context, int index, int realIndex) {
                 Article newsItem = newsItems[index];
                 bool isBookmarked = bookmarkedIndices.contains(index);
-                return Stack(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.6,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).brightness == Brightness.light
-                            ? Colors.grey.shade200
-                            : const Color.fromARGB(255, 16, 8, 29),
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => NewsPage(article: newsItem),
                       ),
-                      child: Column(
-                        children: [
-                          FadeInImage.assetNetwork(
-                            placeholder: 'trendhaber.jpg',
-                            image: newsItem.urlToImage,
-                            fit: BoxFit.cover,
-                            height: 300,
-                            imageErrorBuilder: (context, error, stackTrace) {
-                              return Image.asset(
-                                'trendhaber.jpg',
-                                fit: BoxFit.cover,
-                                height: 300,
-                              );
-                            },
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            newsItem.title,
-                            style: TextStyle(fontSize: 16.0),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 10,
-                      right: 10,
-                      child: IconButton(
-                        icon: Icon(
-                          isBookmarked ? Icons.bookmark : Icons.bookmark_border,
-                          color: isBookmarked ? Colors.grey.shade500 : null,
+                    );
+                  },
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.6,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).brightness == Brightness.light
+                              ? Colors.grey.shade200
+                              : const Color.fromARGB(255, 16, 8, 29),
                         ),
-                        onPressed: () {
-                          setState(() {
-                            if (isBookmarked) {
-                              ref.read(savedNewsProvider.notifier).removeArticle(newsItem);
-                            } else {
-                              ref.read(savedNewsProvider.notifier).addArticle(newsItem);
-                            }
-                            ref.read(bookmarkedProvider.notifier).toggleBookmark(index);
-                          });
-                        },
+                        child: Column(
+                          children: [
+                            FadeInImage.assetNetwork(
+                              placeholder: 'trendhaber.jpg',
+                              image: newsItem.urlToImage,
+                              fit: BoxFit.cover,
+                              height: 300,
+                              imageErrorBuilder: (context, error, stackTrace) {
+                                return Image.asset(
+                                  'trendhaber.jpg',
+                                  fit: BoxFit.cover,
+                                  height: 300,
+                                );
+                              },
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              newsItem.title,
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                      Positioned(
+                        bottom: 10,
+                        right: 10,
+                        child: IconButton(
+                          icon: Icon(
+                            isBookmarked ? Icons.bookmark : Icons.bookmark_border,
+                            color: isBookmarked ? Colors.grey.shade500 : null,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              if (isBookmarked) {
+                                ref.read(savedNewsProvider.notifier).removeArticle(newsItem);
+                              } else {
+                                ref.read(savedNewsProvider.notifier).addArticle(newsItem);
+                              }
+                              ref.read(bookmarkedProvider.notifier).toggleBookmark(index);
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               },
             );
